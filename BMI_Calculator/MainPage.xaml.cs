d6e07a89c250936ae729a8a3d5697b8cb44814eb
Scott Winiarski <Scott1w1@hotmail.com>
2023-10-30 14:47:29 -0500

    Add project files.

---
+++ BMI_Calculator/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BMI_Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            double pounds = Convert.ToDouble(In_weight.Text);
+            double height = 12 * Convert.ToDouble(In_heightFeet.Text) + (Convert.ToDouble(In_heightInches.Text));
+
+            Out_text.Text = Math.Round((pounds / (height * height) * 703), 1).ToString();
+        }
+    }
+}
